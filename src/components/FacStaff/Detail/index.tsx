822933a06ddd04359870cd070e404798d4c99b25
Seth Battis <sbattis@groton.org>
2026-05-24 12:10:32 -0400

    refactor: smaller detail profile photo

--- src/components/FacStaff/Detail/index.tsx
+++ src/components/FacStaff/Detail/index.tsx
@@ -3,10 +3,11 @@ import type { Data } from '@/src/lib/Veracross';
 import { Audience } from '@/src/model/Audience';
 import Link from 'next/link';
 import { Row } from 'react-bootstrap';
-import Address from '../Address';
-import * as NamePronunciation from '../NamePronunciation';
-import Photo from '../Photo';
-import Department from './Department';
+import Address from '../../Address';
+import * as NamePronunciation from '../../NamePronunciation';
+import Photo from '../../Photo';
+import Department from '../Department';
+import './styles.scss';
 
 export type Properties = {
   audience: Audience;
@@ -26,10 +27,10 @@ export async function Detail({
     <>
       <Link href={`/audience/${audience}/directory/facstaff`}>Close</Link>
       <Row>
-        <div className="w-50">
+        <div className="w-33">
           <Photo {...person} />
         </div>
-        <div className="w-50">
+        <div className="w-67">
           <h1>
             {person.full_name} <NamePronunciation.Node {...props} />
           </h1>
