cf2836c5ab629fa1b7417b950cd8e8510b605184
Seth Battis <sbattis@groton.org>
2026-05-26 09:33:35 -0400

    chore: issue tracking

--- app/verify/[audience]/[access]/route.ts
+++ app/verify/[audience]/[access]/route.ts
@@ -7,6 +7,12 @@ export async function GET(
   context: RouteContext<'/verify/[audience]/[access]'>
 ) {
   const { audience, access } = await context.params;
+
+  /*
+   * FIXME: verify embed needs to redirect to a timed refresh page
+   *   Right now the verification expires after an hour and a full refresh of
+   *   the embedding page is required to reconnect.
+   */
   return new NextResponse(null, {
     status: (await verify(audience as Audience, access)) ? 200 : 401
   });
