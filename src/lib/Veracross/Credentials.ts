fb93142a04b1eb6e496d7e9461f800ed8b756456
Seth Battis <sbattis@groton.org>
2026-05-26 13:32:31 -0400

    refactor: make Veracross scopes easier to find/adjust

--- src/lib/Veracross/Credentials.ts
+++ src/lib/Veracross/Credentials.ts
@@ -1,24 +1,9 @@
 import 'server-only';
 
 import { LazySecrets } from '@battis/lazy-secrets';
+import { SCOPES } from './Scopes';
 
 const CREDENTIALS = 'VERACROSS_CREDENTIALS';
-const SCOPES = [
-  'contact_info:list',
-  'contact_info:read',
-  'directory.configurations:list',
-  'directory.household:list',
-  'directory.preferences.household:list',
-  'directory.preferences.household:read',
-  'directory.preferences.people:list',
-  'directory.preferences.people:read',
-  'directory.staff_faculty:list',
-  'directory.student:list',
-  'emergency_contacts:list',
-  'emergency_contacts:read',
-  'households:list',
-  'households:read'
-];
 
 type Credentials = {
   client_id: string;
---
+++ src/lib/Veracross/Scopes.ts
@@ -0,0 +1,16 @@
+export const SCOPES = [
+  'contact_info:list',
+  'contact_info:read',
+  'directory.configurations:list',
+  'directory.household:list',
+  'directory.preferences.household:list',
+  'directory.preferences.household:read',
+  'directory.preferences.people:list',
+  'directory.preferences.people:read',
+  'directory.staff_faculty:list',
+  'directory.student:list',
+  'emergency_contacts:list',
+  'emergency_contacts:read',
+  'households:list',
+  'households:read'
+];
